6d5b0886a94e83dce81a37706c121626a51d4b1f
Marcus Rowcliffe <Marcus.Rowcliffe@ioz.ac.uk>
2018-05-22 18:52:34 +0100

    Updated example to fit new functions

--- CTtracking testing.r
+++ CTtracking testing.r
@@ -1,9 +1,9 @@
+source("C:/Users/rowcliffe.m/Documents/GitHub/CTtracking/CTtracking.r")
 setwd("C:/Users/rowcliffe.m/Documents/CameraTrapping/REM/Calibration/Mytool2018/Gee data")
 
 cdat <- read.poledat("camcal.csv", "pole_id;distance;length")
 ddat <- data.frame(x=3264, y=2449)
 cmod <- cal.cam(cdat, ddat)
-par(mfrow=c(1,2))
 plot(cmod)
 
 #ccdat <- rbind(cdat, cdat)
@@ -13,13 +13,10 @@ plot(cmod)
 #ccmod <- cal.cam(ccdat, ddat)
 #lapply(ccmod, plot)
 
-sdat <- read.poledat("sitecal.csv", "height")
+setwd("./DigiDat")
+pth <- getwd()
+merge.csv(pth, sitecol="CTsite")
+setwd("./merged")
+sdat <- read.poledat("merged.csv", "height")
 smod <- cal.site(cmod, sdat)
-plot(smod)
-
-#ssdat <- rbind(sdat,sdat)
-#ssdat$site_id <- rep(c("a","b"), each=nrow(sdat))
-#ssmod <- cal.site(cmod, ssdat)
-#lapply(ssmod, plot)
-
-
+lapply(smod, plot)
