6ce271d434d295e8b5fcf0892415b13f5c696d49
Marcus Rowcliffe <Marcus.Rowcliffe@ioz.ac.uk>
2018-07-12 00:29:57 +0100

    Added code to to clean up cam_cal file

--- CTtracking testing.r
+++ CTtracking testing.r
@@ -1,8 +1,25 @@
 setwd("C:/Users/rowcliffe.m/Documents/GitHub/CTtracking")
 source("CTtracking.r")
 
+#Sort out cam_cal file
+dat <- read.csv("./Gee data/cam_cal.csv")
+seqan <- strsplit(as.character(dat$sequence_annotation), ";")
+i <- unlist(lapply(seqan, length))==3
+seqan <- seqan[i]
+dat <- dat[i,]
+id <- unlist(lapply(seqan, function(x) paste(x[1],x[2],sep=".")))
+dist <- unlist(lapply(seqan, function(x) x[1]))
+len <- unlist(lapply(seqan, function(x) x[3]))
+dat$sequence_annotation <- paste(id,dist,len, sep=";")
+names(dat)[names(dat)=="Camera_ID"] <- "cam_id"
+names(dat)[names(dat)=="xres"] <- "xdim"
+names(dat)[names(dat)=="yres"] <- "ydim"
+write.csv(dat, "./Gee data/cam_cal2.csv")
+View(dat)
+
 #Extract data for camera calibration model
-cdat <- read.poledat("./Gee data/camcal.csv", "pole_id;distance;length")
+debug(read.poledat)
+cdat <- read.poledat("./Gee data/cam_cal2.csv", "pole_id;distance;length")
 View(cdat)
 
 #Fit camera calibration model(s)
@@ -24,6 +41,7 @@ sctable <- read.csv("./Gee data/cam_table.csv")
 smod <- cal.site(cmod, sdat, sctable)
 
 #Show diagnostic plots
+par(mfrow=c(1,2))
 lapply(smod, plot)
 
 #Predict positions (angle and radius) for animal data
